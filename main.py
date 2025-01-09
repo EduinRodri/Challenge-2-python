05ca21ef77e82aec9089b334551053faa3acb2fd
Ivan Torres <ivan.look3002@gmail.com>
2025-01-09 18:25:18 -0500

    Base de datos terminado

--- main.py
+++ main.py
@@ -31,39 +31,39 @@ MENU_CITAS = '''
 '''
 
 MENU_CLIENTES_VETERINARIOS = '''
-1. Consultar Cliente por ID
-2. Consultar Veterinario por ID
+1. Consultar Cliente
+2. Consultar Veterinario
 3. Modificar Cliente
 4. Modificar Veterinario
 '''
 
 # Clases
 class persona:
-    def __init__(self, nombre, contacto, identidad):
+    def __init__(self, nombre: str, contacto: str, identidad:str):
         self.nombre = nombre
         self.contacto = contacto
         self.id = identidad
 
 
-class cliente(persona):
+class Cliente(persona):
     def __init__(self, nombre, contacto, identidad, dirección):
         super().__init__(nombre, contacto, identidad)
     pass
 
-class veterinario(persona):
+class Veterinario(persona):
     def __init__(self, nombre, contacto, identidad, especialidad, licencia, horario):
         super().__init__(nombre, contacto, identidad)
     pass
 
-class mascota:
+class Mascota:
     def __init__(self, nombre, especie, raza, edad, identidad, dueño):
         pass
 
-class servicio:
+class Servicio:
     def __init__(self, tipo, descripcion, duracion, costo, frecuencia):
         pass
 
-class cita:
+class Cita:
     def __init__(self, fecha, hora, servicio, veterinario, id_mascota):
         pass
 
@@ -74,14 +74,60 @@ def main():
 
 class Datos:
     use = ""
-    tablas = {}
+    tablas: dict[str, list[persona]] = {
+        "clientes": [],
+        "veterinarios": [],
+        "mascotas": [],
+        "servicios": [],
+    }
     def __init__(self):
         with open("datos.txt", "r") as datos:
             text = datos.read()
             if text != "":
                 tablasContenido = text.split(SEPARADOR_TABLA)
-                for i in range(len(tablasContenido)):
-                    tabla = tablasContenido[i]
-                    titulo = tabla
+                for tabla in tablasContenido:
+                    titulo = tabla.split(TITULO)[0]
+                    contenidoTexto = tabla.split(TITULO)[1]
+                    contenido = contenidoTexto.split(SEPARADOR_ELEMENTO)
+                    for elemento in contenido:
+                        propiedades = elemento.split(PROPIEDAD)
+                        if titulo == "cliente":
+                            cliente = Cliente(propiedades[0], propiedades[1], propiedades[2], propiedades[3])
+                            self.tablas["clientes"].append(cliente)
+                        elif titulo == "veterinario":
+                            veterinario = Veterinario(propiedades[0], propiedades[1], propiedades[2], propiedades[3], propiedades[4], propiedades[5])
+                            self.tablas["veterinarios"].append(veterinario)
+                        elif titulo == "mascota":
+                            mascota = Mascota(propiedades[0], propiedades[1], propiedades[2], propiedades[3], propiedades[4], propiedades[5])
+                            self.tablas["mascotas"].append(mascota)
+                        elif titulo == "servicio":
+                            servicio = Servicio(propiedades[0], propiedades[1], propiedades[2], propiedades[3], propiedades[4])
+                            self.tablas["servicios"].append(servicio)
+                        pass
+                    
+                    
         pass
     pass
+
+    def obtener(self, id: int):
+        result = self.tablas[self.use][id]
+        return result
+    
+    def guardar(self):
+        with open("datos.txt", "w") as datos:
+            for key in self.tablas:
+                texto = key + TITULO
+                for elemento in self.tablas[key]:
+                    for propiedad in elemento:
+                        texto += propiedad + PROPIEDAD
+                    pass
+                    texto += SEPARADOR_ELEMENTO
+                pass
+                texto += SEPARADOR_TABLA
+                datos.write(texto)
+            pass
+        pass
+
+    def eliminar(self, id: int):
+        self.tablas[self.use].pop(id)
+        pass
